bfd7c65c74a139aaed9c3276b0aa8a32a634950b
cdsto <cdstovall@gmail.com>
2018-01-18 15:05:19 -0600

    Exercise 2.3

--- ch02/Date.java
+++ ch02/Date.java
@@ -7,6 +7,8 @@ public class Date
         String month = "January";
         int year = 2018;
 
+        System.out.println("American format:");
+
         System.out.print(day);
         System.out.print(", ");
         System.out.print(month);
@@ -14,6 +16,8 @@ public class Date
         System.out.print(date);
         System.out.print(", ");
         System.out.println(year);
+
+        System.out.println("European format:");
 
         System.out.print(day);
         System.out.print(" ");
