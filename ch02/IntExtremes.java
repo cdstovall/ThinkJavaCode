2c4462c4aa2b82e4009b2559e46ab863387b9a55
cdsto <cdstovall@gmail.com>
2018-01-18 15:45:08 -0600

    Exercise 2-4

---
+++ ch02/IntExtremes.java
@@ -0,0 +1,22 @@
+public class IntExtremes
+{
+    public static void main(String args[])
+    {
+        int postiveInt = 2147483647;
+
+        System.out.println(postiveInt);
+
+        postiveInt = 2147483647 + 1;
+
+        System.out.println(postiveInt);
+
+        int negativeInt = -2147483648;
+
+        System.out.println(negativeInt);
+
+        negativeInt = -2147483648 -1;
+
+        System.out.println(negativeInt);
+
+    }
+}
