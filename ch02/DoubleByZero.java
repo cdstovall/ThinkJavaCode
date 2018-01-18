6992cd13597e2c98575f47071ab8c4b752d7bc58
cdsto <cdstovall@gmail.com>
2018-01-18 15:58:18 -0600

    Exercise 2-5

---
+++ ch02/DoubleByZero.java
@@ -0,0 +1,11 @@
+public class DoubleByZero
+{
+    public static void main(String[] args)
+    {
+        double a = 42.0;
+        double b = 0.0;
+        double result = a / b;
+
+        System.out.println(result);
+    }
+}
