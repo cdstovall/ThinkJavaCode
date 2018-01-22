48aaf73778be82d31b0285bf9348a6eacb845684
cdsto <cdstovall@gmail.com>
2018-01-22 11:03:58 -0600

    Exercise 5-B

--- ch05/LogicMethods.java
+++ ch05/LogicMethods.java
@@ -2,14 +2,35 @@ public class LogicMethods
 {
     public static void main(String[] args)
     {
-
+        printIsLargeOrSmall(5);
+        printIsLargeOrSmall(100);
+        printIsLargeOrSmall(50);
     }
 
-    private static void printIsLarge(int number)
+    //5-A
+    /*private static void printIsLarge(int number)
     {
         if (number > 99)
         {
             System.out.println("The number is large");
+
+            System.out.println();
+        }
+    }*/
+
+    //5-B
+    private static void printIsLargeOrSmall(int number)
+    {
+        if (number > 99)
+        {
+            System.out.println("The number is large");
+
+            System.out.println();
+        }
+
+        if (number < 10)
+        {
+            System.out.println("The number is small");
 
             System.out.println();
         }
