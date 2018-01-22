901eea42fafde2eec44c6f9f6e6bd4c525aba6a7
cdsto <cdstovall@gmail.com>
2018-01-22 11:45:17 -0600

    Exercise 5-D

--- ch05/LogicMethods.java
+++ ch05/LogicMethods.java
@@ -2,9 +2,12 @@ public class LogicMethods
 {
     public static void main(String[] args)
     {
-        printLargest(5,6);
-        printLargest(6,5);
-        printLargest(6,6);
+        printLargestOdd(5,7);
+        printLargestOdd(7,5);
+        printLargestOdd(5,8);
+        printLargestOdd(8,5);
+        printLargestOdd(8,8);
+        printLargestOdd(5,5);
     }
 
     //5-A
@@ -37,7 +40,7 @@ public class LogicMethods
     }*/
 
     //5-C
-    private static void printLargest( int number1, int number2)
+    /*private static void printLargest(int number1, int number2)
     {
 
         if (number1 > number2)
@@ -59,6 +62,53 @@ public class LogicMethods
             System.out.println("The numbers are equal");
             System.out.println();
 
+        }
+    } */
+
+    private static void printLargestOdd(int number1, int number2)
+    {
+        int remainder1 = (number1 % 2);
+        int remainder2 = (number2 % 2);
+
+        if (remainder1 == 1 && remainder2 == 1)
+        {
+            if (number1 > number2)
+            {
+                System.out.println("The largest odd number is: " + number1);
+
+                System.out.println();
+            }
+
+            else if (number2 > number1) {
+
+                System.out.println("The largest odd number is: " + number2);
+
+                System.out.println();
+            }
+
+            else if (number1 == number2) {
+
+                System.out.println("Two odds make an even");
+                System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
+
+            }
+        }
+
+        else if (remainder1 == 1 && remainder2 == 0)
+        {
+            System.out.println("The largest odd number is: " + number1);
+            System.out.println();
+        }
+
+        else if (remainder1 == 0 && remainder2 == 1)
+        {
+            System.out.println("The largest odd number is: " + number2);
+            System.out.println();
+        }
+
+        else {
+            System.out.println("Neither number is odd");
+            System.out.println();
         }
     }
 
