3e709cc6f9e9798f9515ca58a833ea6c5d73269e
cdsto <cdstovall@gmail.com>
2018-01-22 10:58:15 -0600

    Exercise 5-A

---
+++ ch05/LogicMethods.java
@@ -0,0 +1,18 @@
+public class LogicMethods
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static void printIsLarge(int number)
+    {
+        if (number > 99)
+        {
+            System.out.println("The number is large");
+
+            System.out.println();
+        }
+    }
+
+}
