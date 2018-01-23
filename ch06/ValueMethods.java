d3175d18faa8da73255c06ff65354c9ca4d17cc3
cdsto <cdstovall@gmail.com>
2018-01-23 09:48:59 -0600

    Exercise 6.3

--- ch06/ValueMethods.java
+++ ch06/ValueMethods.java
@@ -2,14 +2,24 @@ public class ValueMethods
 {
     public static void main(String[] args)
     {
-        System.out.println(isDivisible(4, 2));
-        System.out.println(isDivisible(5, 2));
-
+        System.out.println(isTriangle(3,4,5));
+        System.out.println(isTriangle(1,1,12));
     }
+
+    //Exercise 6.3
+    private static boolean isTriangle (int a, int b, int c)
+    {
+        return (a <= b + c && b <= a + c && c <= a + b);
+    }
+
+
+
+
+
 
     //Exercise 6.2
-    private static boolean isDivisible(int n, int m)
+    /* private static boolean isDivisible(int n, int m)
     {
         return (n % m == 0);
-    }
+    } */
 }
