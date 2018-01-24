9d809f75c82f16c844af5495f133f14cdcd38384
cdsto <cdstovall@gmail.com>
2018-01-24 14:19:56 -0600

    Exercise 7-D

---
+++ ch07/MyLoopsD.java
@@ -0,0 +1,16 @@
+public class MyLoopsD
+{
+    public static void main(String[] args)
+    {
+        exercise7d(10);
+    }
+
+    private static void exercise7d(int x)
+    {
+        int y = 1;
+        while (y <= x){
+            System.out.println(y);
+            y++;
+        }
+    }
+}
