a82bf97236cff52ec9b5fd8207d4cbdfea8ddeb8
cdsto <cdstovall@gmail.com>
2018-01-26 14:52:57 -0600

    Exercise 8-A

---
+++ ch08/Exercise8A.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Exercise8A
+{
+    public static void main(String[] args)
+    {
+        int testArray[] = {1,2,3,55,4,5,6};
+
+        printArray(testArray);
+    }
+
+    private static void printArray(int[] y)
+    {
+        System.out.println(Arrays.toString(y));
+
+    }
+}
