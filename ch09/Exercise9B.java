f7f2ec991ba6294af28a90795cafa08db8293cc3
cdsto <cdstovall@gmail.com>
2018-01-29 15:24:26 -0600

    Exercise 9-B

---
+++ ch09/Exercise9B.java
@@ -0,0 +1,16 @@
+public class Exercise9B
+{
+    public static void main(String[] args)
+    {
+        printLastCharacter("Hello");
+        printLastCharacter("Goodbye");
+
+    }
+
+    private static void printLastCharacter(String text)
+    {
+        System.out.println("Print the last character in " + text);
+
+        System.out.println(text.charAt(text.length() - 1));
+    }
+}
