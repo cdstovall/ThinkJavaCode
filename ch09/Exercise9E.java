4611e4e10e7123f8f07d8f1945b9c0edd94b1af3
cdsto <cdstovall@gmail.com>
2018-01-29 16:20:26 -0600

    Exercise 9-E

---
+++ ch09/Exercise9E.java
@@ -0,0 +1,14 @@
+public class Exercise9E
+{
+    public static void main(String[] args)
+    {
+        printFirstThree("Hello");
+        printFirstThree("Goodbye");
+
+    }
+
+    private static void printFirstThree(String text)
+    {
+        System.out.println(text.substring(0, 3));
+    }
+}
