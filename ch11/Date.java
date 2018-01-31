8bf6f907bac647c3995528de267299292b5041cd
cdsto <cdstovall@gmail.com>
2018-01-31 14:53:07 -0600

    Exercise 11-A

---
+++ ch11/Date.java
@@ -0,0 +1,14 @@
+public class Date
+{
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int a, int b, int c)
+    {
+        day = a;
+        month = b;
+        year = c;
+    }
+
+}
